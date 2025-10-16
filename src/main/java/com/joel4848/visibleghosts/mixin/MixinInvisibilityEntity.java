93bee3d49a68df47e94cea159ddf4ec84696bc63
Joel Taylor <joel.w.taylor@gmail.com>
2025-10-16 14:42:28 +0100

    Added render invisible entities feature. Updated licence.md

---
+++ src/main/java/com/joel4848/visibleghosts/mixin/MixinInvisibilityEntity.java
@@ -0,0 +1,14 @@
+package com.joel4848.visibleghosts.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(net.minecraft.entity.Entity.class)
+public class MixinInvisibilityEntity {
+    @Inject(method = "isInvisibleTo", at = @At("HEAD"), cancellable = true)
+    private void overrideIsInvisibleToPlayer(net.minecraft.entity.player.PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+}
