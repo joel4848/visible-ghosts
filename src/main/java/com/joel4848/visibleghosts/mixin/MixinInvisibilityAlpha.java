9375785e204789388397dfddb9ddaf0fcdc6fc8b
Joel Taylor <joel.w.taylor@gmail.com>
2025-10-16 14:06:30 +0100

    Updated mixin to work with 1.21.1

--- src/main/java/com/joel4848/visibleghosts/mixin/MixinInvisibilityAlpha.java
+++ src/main/java/com/joel4848/visibleghosts/mixin/MixinInvisibilityAlpha.java
@@ -1,6 +1,7 @@
 package com.joel4848.visibleghosts.mixin;
 
 import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -8,11 +9,10 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(LivingEntityRenderer.class)
 public class MixinInvisibilityAlpha {
     @ModifyConstant(
-            method = "render",
-            constant = @Constant(floatValue = 0.15F, ordinal = 0),
-            require = 0
+            method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            constant = @Constant(intValue = 654311423)
     )
-    private float overrideInvisibilityAlpha(float original) {
-        return 0.65F;
+    private int foo(int constant) {
+        return ColorHelper.Argb.getArgb(166, 255, 255, 255);
     }
 }
